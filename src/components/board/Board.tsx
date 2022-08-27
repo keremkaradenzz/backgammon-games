7dbb22b18abbb9448228ba4c064e057071f1d4c3
Kerem Karadeniz <keremkaradenzz@gmail.com>
2022-08-27 19:36:18 +0300

    game props updated

--- src/components/board/Board.tsx
+++ src/components/board/Board.tsx
@@ -14,8 +14,8 @@ const Board: React.FC<IProps> = ({ gameData }) => {
         <div className='board-first' style={{ transform: 'rotate(180deg)', marginTop: -100 }}>
           {
             gameData.map((game, i) => i < 12 && (i % 2 === 0 ?
-              <BoardLine bgcolor='black' count={game.haveStone.length} /> :
-              <BoardLine bgcolor='red' count={game.haveStone.length} />
+              <BoardLine bgcolor='black' count={game.haveStone.length} game={game} /> :
+              <BoardLine bgcolor='red' count={game.haveStone.length} game={game}/>
             ))}
         </div>
 
@@ -23,8 +23,8 @@ const Board: React.FC<IProps> = ({ gameData }) => {
         <div className='board-first' style={{ marginTop: 200 }} >
           {
             gameData.map((game, i) => i >= 12 && (i % 2 === 0 ?
-              <BoardLine bgcolor='black' count={game.haveStone.length} /> :
-              <BoardLine bgcolor='red' count={game.haveStone.length} />
+              <BoardLine bgcolor='black' count={game.haveStone.length} game={game}/> :
+              <BoardLine bgcolor='red' count={game.haveStone.length} game={game} />
             ))}
         </div>
 
--- src/components/boardLine/BoardLine.tsx
+++ src/components/boardLine/BoardLine.tsx
@@ -1,31 +1,32 @@
 import React from 'react'
 import Stone from '../stone/Stone';
 import './index.scss';
-
+import { IData } from '../../utils/types';
 interface IBoardLineProps {
   bgcolor: string;
   count?: number;
+  game: IData;
 }
 const BoardLine: React.FC<IBoardLineProps> = ({ bgcolor, count = 0 }) => {
   const newArray = Array(count).fill('');
 
-  function handleDrop(e:React.DragEvent){
-     e.preventDefault();
-     console.log(e.target);
+  function handleDrop(e: React.DragEvent) {
+    e.preventDefault();
+    console.log(e.target);
   }
-  function handleDragOver(e:React.DragEvent){
+  function handleDragOver(e: React.DragEvent) {
     e.preventDefault();
     //console.log(e.target);
- }
+  }
   return (
-      <div className='line' onDragOver={handleDragOver} onDrop={e=> handleDrop(e)} style={{ background:bgcolor,borderColor: `transparent transparent ${bgcolor} transparent` }}>
-        {
-          newArray.map((i,index)=> {
-            return <Stone  key={index}/>
-          })
-        }
-      </div>
-   
+    <div className='line' onDragOver={handleDragOver} onDrop={e => handleDrop(e)} style={{ background: bgcolor, borderColor: `transparent transparent ${bgcolor} transparent` }}>
+      {
+        newArray.map((i, index) => {
+          return <Stone key={index} />
+        })
+      }
+    </div>
+
   )
 }
 
